c38fe8bf4b4e6d617f0d0d8c6c5d18d17761feae
Andrew Bihl <andrewbihlva@gmail.com>
2020-06-14 14:18:42 -0400

    Separate Course into its own component for course-information-continued

--- part2/course-information-continued/src/index.js
+++ part2/course-information-continued/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Course from "./components/course"
 
 const App = () => {
   const courses = [
@@ -46,41 +47,5 @@ const App = () => {
   );
 };
 
-const Course = ({ course }) => {
-  return (
-    <>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </>
-  );
-};
-
-const Header = ({ course }) => {
-  return <h1>{course.name}</h1>;
-};
-
-const Content = ({ course }) => {
-  return (
-    <div>
-      <Part part={course.parts[0]} />
-      <Part part={course.parts[1]} />
-      <Part part={course.parts[2]} />
-    </div>
-  );
-};
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part.name} {props.part.exercises}
-    </p>
-  );
-};
-
-const Total = ({ course }) => {
-  const sum = course.parts.reduce((total, part) => total+part.exercises, 0)
-  return <p><strong>Number of exercises: {sum}</strong></p>;
-};
 
 ReactDOM.render(<App />, document.getElementById("root"));
