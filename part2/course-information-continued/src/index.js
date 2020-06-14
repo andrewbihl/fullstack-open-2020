f8ea78a292465d751a0931804f796103a7be5bce
Andrew Bihl <andrewbihlva@gmail.com>
2020-06-14 14:05:57 -0400

    use .map( () => () ) syntax to permit multiple courses to be display

--- part2/course-information-continued/src/index.js
+++ part2/course-information-continued/src/index.js
@@ -2,27 +2,46 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
-  };
+  const courses = [
+    {
+      name: "Half Stack application development",
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+        },
+      ],
+    },
+    {
+      name: "CPR Training",
+      parts: [
+        {
+          name: "Handling the situation",
+          exercises: 6,
+        },
+        {
+          name: "Compressions and breaths",
+          exercises: 4,
+        },
+        {
+          name: "Children and infants",
+          exercises: 3,
+        },
+      ],
+    },
+  ];
 
   return (
     <div>
-      <Course course={course} />
+      {courses.map(c => <Course course={c} key={c.name} />)} 
     </div>
   );
 };
@@ -66,6 +85,5 @@ const Total = ({ course }) => {
     course.parts[2].exercises;
   return <p>Number of exercises {sum}</p>;
 };
-
 
 ReactDOM.render(<App />, document.getElementById("root"));
