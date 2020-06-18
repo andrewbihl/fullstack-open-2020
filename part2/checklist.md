b70ebf97585b5f0e36e5545f40bb66c26757e1d4
Andrew Bihl <andrewbihlva@gmail.com>
2020-06-17 23:38:27 -0400

    finish countries step 2

--- part2/checklist.md
+++ part2/checklist.md
@@ -10,5 +10,5 @@
 - [x] 2.10: phonebook, step 5
 - [x] 2.11: phonebook, step 6
 - [x] 2.12: countries, step 1
-- [ ] 2.13: countries, step 2
+- [x] 2.13: countries, step 2
 - [ ] 2.14: countries, step 3
--- part2/countries/src/components/ResultList.js
+++ part2/countries/src/components/ResultList.js
@@ -1,12 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ResultList = (props) => {
   const { countries } = { ...props };
 
   if (countries.length === 1) {
-      return (
-          <CountryExpanded country ={countries[0]}/>
-      )
+    return <CountryResult country={countries[0]} expanded={true} />;
   }
 
   if (countries.length <= 10)
@@ -18,33 +16,43 @@ const ResultList = (props) => {
       </>
     );
 
-    return <p>too many results ya shit</p>
+  return <p>too many results ya shit</p>;
 };
 
 const CountryExpanded = (props) => {
-    const {country} = {...props}
-
-    return (
-        <>
-            <h2>{country.name}</h2>
-            <p>capital: {country.capital}
-            <br/>
-            population: {country.population}</p>
-            <h3>languages</h3>
-            <ul>
-                {country.languages.map(lang => (
-                    <li id={lang.name}>{lang.name}</li>
-                ))}
-            </ul>
-            <img src={country.flag}></img>
-        </>
-    )
-}
-
-const CountryResult = (props) => {
   const { country } = { ...props };
 
-  return <p key={country.name}>{country.name}</p>;
+  return (
+    <>
+      <h2>{country.name}</h2>
+      <p>
+        capital: {country.capital}
+        <br />
+        population: {country.population}
+      </p>
+      <h3>languages</h3>
+      <ul>
+        {country.languages.map((lang) => (
+          <li id={lang.name}>{lang.name}</li>
+        ))}
+      </ul>
+      <img src={country.flag} width="25%"></img>
+    </>
+  );
+};
+
+const CountryResult = (props) => {
+  const { country, expandedInitially } = { ...props };
+
+  const [expanded, setExpanded] = useState(expandedInitially)
+
+  return expanded ? (
+    <CountryExpanded key={country.name} country={country}></CountryExpanded>
+  ) : (
+    <p key={country.name}>{country.name}
+    <button onClick={() => {setExpanded(true)}}>Show!</button>
+    </p>
+  );
 };
 
 export default ResultList;
